f5b8c92a59572b58e91eeab06054964320ab7f90
Chanbin Na <chanbinna@gmail.com>
2025-05-27 23:35:41 -0700

    second part

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab8-Starter
+
+## How are graceful degradation and service workers related?
+Grateful degradation is when we start with max technology and hopefully address lower levels with grace. 
+Service workers support this by allowing the app to work offline using cached content and by handling network failures without breaking the user experience.
+
+Link:
+
+![pwa](/pwa.png)
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,14 +45,30 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  if ('serviceWorker' in navigator) {
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', async () => {
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      try {
+        const registration = await navigator.serviceWorker.register('./sw.js', {
+          scope: '/',
+        });
+
+        console.log('Service Worker successfully registered');
+      } catch (error) {
+        console.log('Servioce Worker registration failed:', error)
+      }
+      // B4. TODO - Once the service worker has been successfully registered, console
+      //            log that it was successful.
+      // B5. TODO - In the event that the service worker registration fails, console
+      //            log that it has failed.
+
+    });
+  }
+
+
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
@@ -116,7 +132,7 @@ async function getRecipes() {
         }
       } catch (err) {
         // A10. TODO - Log any errors from catch using console.error
-        console.error('Error fetching recipe: ', err);
+        console.error('Error fetching recipe:', err);
         // A11. TODO - Pass any errors to the Promise's reject() function
         reject(err);
       }
